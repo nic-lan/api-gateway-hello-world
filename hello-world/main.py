205faa7e22bc09eaae2d5be6245c458129a9f424
Nicolas Languille <me@niclan.io>
2023-12-22 20:56:32 +0000

    add lint step

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,10 +1,11 @@
 import json
 
+
 def lambda_handler(_event, _context):
     return {
-        'statusCode': 200,
-        'headers': {
-          'Content-Type': 'application/json',
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
         },
-        'body': json.dumps({'message': 'Hello, world!'})
+        "body": json.dumps({"message": "Hello, world!"}),
     }
--- hello-world/test/test_main.py
+++ hello-world/test/test_main.py
@@ -1,23 +1,25 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import unittest
 import json
 from main import lambda_handler
 
+
 class TestLambdaHandler(unittest.TestCase):
-  def test_lambda_handler(self):
-    expected_result = {
-      'statusCode': 200,
-      'headers': {
-        'Content-Type': 'application/json',
-      },
-      'body': json.dumps({'message': 'Hello, world!'})
-    }
+    def test_lambda_handler(self):
+        expected_result = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps({"message": "Hello, world!"}),
+        }
 
-    # Call the function with dummy event and context
-    result = lambda_handler({}, {})
+        # Call the function with dummy event and context
+        result = lambda_handler({}, {})
 
-    # Assert that the function returns the expected result
-    self.assertEqual(result, expected_result)
+        # Assert that the function returns the expected result
+        self.assertEqual(result, expected_result)
