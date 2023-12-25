d63eb002a4376caf3545dac67fe06216784c0026
Nicolas Languille <me@niclan.io>
2023-12-25 23:41:22 +0000

    limit api-infra terraform privilegies

--- main-infra/main.tf
+++ main-infra/main.tf
@@ -60,3 +60,110 @@ resource "aws_dynamodb_table" "dynamodb_terraform_state_lock" {
   #   prevent_destroy = true
   # }
 }
+
+resource "aws_iam_role" "terraform_api_role" {
+  name = "terraform_api_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role_policy" "terraform_api_role_policy" {
+  name = "s3_policy"
+  role = aws_iam_role.terraform_api_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "s3:ListBucket"
+        Resource = "${aws_s3_bucket.terraform_state.arn}"
+      },
+      {
+        Effect = "Allow",
+        Action = "s3:ListAllMyBuckets"
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["s3:*"],
+        Resource = "arn:aws:s3:::hello-world*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["lambda:*"],
+        Resource = "arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:HelloWorld"
+      },
+      {
+        Effect = "Allow",
+        Action = ["logs:*"],
+        Resource = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group*"
+      },
+      {
+        Effect = "Allow",
+        Action = "logs:CreateLogDelivery",
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "iam:*"
+        ],
+        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/serverless_lambda*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["apigateway:*"],
+        Resource = "arn:aws:apigateway:${var.aws_region}::/apis*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+        Resource = "${aws_s3_bucket.terraform_state.arn}/hello-world-api/terraform.tfstate"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:DescribeTable",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ],
+        Resource = "${aws_dynamodb_table.dynamodb_terraform_state_lock.arn}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user" "terraform_api_user" {
+  name = "terraform_api_user"
+  path = "/"
+}
+
+resource "aws_iam_policy" "terraform_api_policy" {
+  name        = "terraform_api_policy"
+  path        = "/"
+  description = "Policy for Terraform API User"
+  policy      = aws_iam_role_policy.terraform_api_role_policy.policy
+}
+
+resource "aws_iam_user_policy_attachment" "user_policy_attachment" {
+  user       = aws_iam_user.terraform_api_user.name
+  policy_arn = aws_iam_policy.terraform_api_policy.arn
+}
