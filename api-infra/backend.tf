34c7bcb38d06f22c7073daf9ecbb606cb3b18d0e
Nicolas Languille <nic-lan@users.noreply.github.com>
2023-12-25 23:45:25 +0000

    limit api-infra terraform privilegies (#5)

--- api-infra/backend.tf
+++ api-infra/backend.tf
@@ -1,13 +1,13 @@
 
 resource "random_pet" "random-pet" {
-  prefix = "hello-world"
+  prefix = "hello-world-api"
   length = 4
 }
 
 terraform {
   backend "s3" {
     bucket         = "bucket-name-placeholder"  # This will be overridden by -backend-config
-    key            = "hello-world/terraform.tfstate"
+    key            = "hello-world-api/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "my-lock-table"
     encrypt        = true
